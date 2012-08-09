04b2af3439690f4dedfa6580b3c0ec54f15e30ff
Travis Harrison <guumby@gmail.com>
2012-08-08 21:58:24 -0500

    more functionality

--- qc.py
+++ qc.py
@@ -1,70 +1,74 @@
 #!/usr/bin/env python
 
-import os, sys, copy, pprint
-import urllib, urllib2, json
+import math
 import flotplot
+from ipyTools import *
 
-API_URL = 'http://api.metagenomics.anl.gov/'
-
-def obj_from_url(url):
-    try:
-        req = urllib2.Request(url, headers={'Accept': 'application/json'})
-        res = urllib2.urlopen(req)
-    except urllib2.HTTPError, error:
-        print "ERROR: ", error.read()
-        return None
-    if not res:
-        print "ERROR: no results returned"
-        return None
-    obj = json.loads(res.read())
-    if not obj:
-        print "ERROR: return structure not valid json format"
-        return None
-    return obj
+PLOT = flotplot.Plot()
 
 class QC:
-    def __init__(self, mgid):
-        self.mgid = mgid
-        self.drisee = Drisee(mgid)
-        self.kmer = Kmer(mgid)
-        self.bp_histo = NucleoProfile(mgid)
+    def __init__(self, aID):
+        self.ID       = aID
+        self.drisee   = Drisee(aID=self.ID)
+        self.kmer     = Kmer(self.ID)
+        self.bp_histo = NucleoProfile(self.ID)
 
 class Drisee:
-    def __init__(self, mgid):
-        self.mgid = mgid
-        self.data = self.get_drisee()
-        self.error = self.data['errors'] if self.data and ('error' in self.data) else None
-        self.profile = self.data['count_profile'] if self.data and ('count_profile' in self.data) else None
+    def __init__(self, aID=None, data=None):
+        if aID:
+            self.ID   = aID
+            self.data = self.get_drisee()
+        if data:
+            self.data = data
+        self.error   = self.data['errors'] if self.data and ('errors' in self.data) else None
+        self.count   = self.data['count_profile'] if self.data and ('count_profile' in self.data) else None
+        self.percent = self.data['percent_profile'] if self.data and ('percent_profile' in self.data) else None
+        if self.count and (not self.percent):
+            self.percent = self.count_to_percent()
         
     def get_drisee(self):
-        return obj_from_url(API_URL+'drisee/'+self.mgid)
+        return obj_from_url(API_URL+'drisee/'+self.ID)
+
+    def count_to_percent(self):
+        if not self.count:
+            return None
+        rows = []
+        data = []
+        for i in range(self.count['rows']):
+            if i < 50:
+                continue
+            cRow  = self.count['data'][i]
+            total = sum(cRow)
+            percs = map(lambda x: 100 * ((x * 1.0) / total), cRow)
+            rows.append(i)
+            data.append([ percs[6:11], sum(percs[6:11]) ])
+        return {'rows': rows, 'columns': ['A','T','C','G','N','InDel','Total'], 'data': data}
 
     def plot_drisee(self):
-        if not (self.data and ('percent_profile' in self.data)):
+        if not self.percent:
             return None
-        x = self.data['percent_profile']['rows']
-        l = self.data['percent_profile']['columns']
-        yA = map(lambda y: y[0], self.data['percent_profile']['data'])
-        yT = map(lambda y: y[1], self.data['percent_profile']['data'])
-        yC = map(lambda y: y[2], self.data['percent_profile']['data'])
-        yG = map(lambda y: y[3], self.data['percent_profile']['data'])
-        yN = map(lambda y: y[4], self.data['percent_profile']['data'])
-        yX = map(lambda y: y[5], self.data['percent_profile']['data'])
-        yTot = map(lambda y: y[6], self.data['percent_profile']['data'])
-        plt = flotplot.Plot()
-        plt.legendloc ='nw'
-        return plt.plot_figure([x,x,x,x,x,x,x],[yA,yT,yC,yG,yN,yX,yTot],label=l)
+        x = self.percent['rows']
+        l = self.percent['columns']
+        yA = map(lambda y: y[0], self.percent['data'])
+        yT = map(lambda y: y[1], self.percent['data'])
+        yC = map(lambda y: y[2], self.percent['data'])
+        yG = map(lambda y: y[3], self.percent['data'])
+        yN = map(lambda y: y[4], self.percent['data'])
+        yX = map(lambda y: y[5], self.percent['data'])
+        yTot = map(lambda y: y[6], self.percent['data'])
+        PLOT.legendloc = 'nw'
+        PLOT.plot_figure([x,x,x,x,x,x,x],[yA,yT,yC,yG,yN,yX,yTot],label=l)
 
 class NucleoProfile:
-    def __init__(self, mgid):
-        self.mgid = mgid
-        self.data = self.get_bp_profile()
+    def __init__(self, aID):
+        self.ID      = aID
+        self.data    = self.get_bp_profile()
         self.profile = self.data['counts'] if self.data and ('counts' in self.data) else None
 
     def get_bp_profile(self):
-        return obj_from_url(API_URL+'bp_histogram/'+self.mgid)
+        return obj_from_url(API_URL+'bp_histogram/'+self.ID)
 
-    def plot_bps:
+    def plot_bps(self):
         if not (self.data and ('percents' in self.data)):
             return None
         x = self.data['percents']['rows']
@@ -74,24 +78,80 @@ class NucleoProfile:
         yC = map(lambda y: y[2], self.data['percents']['data'])
         yG = map(lambda y: y[3], self.data['percents']['data'])
         yN = map(lambda y: y[4], self.data['percents']['data'])
-        plt = flotplot.Plot()
-        plt.legendloc ='nw'
-        return plt.plot_figure([x,x,x,x,x],[yA,yT,yC,yG,yN],label=l)
+        PLOT.legendloc = 'se'
+        PLOT.plot_figure([x,x,x,x,x],[yA,yT,yC,yG,yN],label=l)
 
 class Kmer:
-    def __init__(self, mgid):
-        self.mgid = mgid
+    def __init__(self, aID):
+        self.ID   = aID
         self.data = self.get_kmer()
 
     def get_kmer(self):
-        return obj_from_url(API_URL+'kmer/'+self.mgid)
+        return obj_from_url(API_URL+'kmer/'+self.ID)
 
     def plot_kmer_abundance(self):
-        return None
+        if not (self.data and ('profile' in self.data)):
+            return None
+        x = map(lambda z: math.log(z[3], 10), self.data['profile'])
+        y = map(lambda z: math.log(z[0], 10), self.data['profile'])
+        PLOT.legendloc = 'sw'
+        PLOT.plot_figure(x,y,label='kmer rank abundance')
 
     def plot_kmer_ranked(self):
-        return None
+        if not (self.data and ('profile' in self.data)):
+            return None
+        x = map(lambda z: math.log(z[3], 10), self.data['profile'])
+        y = map(lambda z: 1 - (1.0 * z[5]), self.data['profile'])
+        PLOT.legendloc = 'sw'
+        PLOT.plot_figure(x,y,label='ranked kmer consumed')
 
     def plot_kmer_spectrum(self):
+        if not (self.data and ('profile' in self.data)):
+            return None
+        x = map(lambda z: math.log(z[0], 10), self.data['profile'])
+        y = map(lambda z: math.log(z[1], 10), self.data['profile'])
+        PLOT.legendloc = 'sw'
+        PLOT.plot_figure(x,y,label='kmer spectrum')
+
+def merge_drisee_profile(qc_set, profile='count'):
+    if profile == 'count':
+        columns = qc_set[0].drisee.count['columns']
+        colMax  = len(columns)
+        rowMax  = max(map(lambda x: len(x.drisee.count['rows']), qc_set))
+        rows    = map(lambda x: x+1, range(rowMax))
+        mMatrix = [[0 for i in range(colMax)] for j in range(rowMax)]
+        for qc in qc_set:
+            if not qc.drisee.count:
+                continue
+            curLen = len(qc.drisee.count['rows'])
+            for r in range(rowMax):
+                if r == curLen:
+                    break
+                for c in range(colMax):
+                    mMatrix[r][c] += qc.drisee.count['data'][r][c]
+        mData = {'count_profile': {'rows': rows, 'columns': columns, 'data': mMatrix}}
+        return Drisee(data=mData)
+    elif profile == 'percent':
+        columns = qc_set[0].drisee.percent['columns']
+        colMax  = len(columns)
+        rowMax  = max(map(lambda x: len(x.drisee.percent['rows']), qc_set))
+        rows    = map(lambda x: x+51, range(rowMax))
+        rowNums = [0 for i in range(rowMax)]
+        mMatrix = [[0 for i in range(colMax)] for j in range(rowMax)]
+        for qc in qc_set:
+            if not qc.drisee.percent:
+                continue
+            curLen = len(qc.drisee.percent['rows'])
+            for r in range(rowMax):
+                if r == curLen:
+                    break
+                rowNums[r] += 1
+                for c in range(colMax):
+                    mMatrix[r][c] += qc.drisee.count['data'][r][c]
+        for r in range(rowMax):
+            for c in range(colMax):
+                mMatrix[r][c] = mMatrix[r][c] / rowNums[r]
+        mData = {'percent_profile': {'rows': rows, 'columns': columns, 'data': mMatrix}}
+        return Drisee(data=mData)
+    else:
         return None
-    
