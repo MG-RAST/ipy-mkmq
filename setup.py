21feb16866271826180bc30355aa8d550d3fd851
Travis Harrison <guumby@gmail.com>
2012-08-14 11:32:50 -0700

    setup script

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+import ipyQMQC
+
+setup(
+    name='ipyQMQC',
+    version=ipyQMQC.__version__,
+    description=ipyQMQC.__description__,
+    author=ipyQMQC.__author__,
+    url='https://github.com/teharrison/ipy-qmqc',
+    packages=['ipyQMQC'],
+    )
