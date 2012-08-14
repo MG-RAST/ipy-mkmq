0e3dc764ea4fc0168e8831d39dfec38bee530983
Travis Harrison <guumby@gmail.com>
2012-08-14 11:59:47 -0700

    more changes

--- ipyQMQC/__init__.py
+++ ipyQMQC/__init__.py
@@ -1,1 +1,8 @@
+"""
+prototype ipy tools.
+
+"""
+__author__ = 'Travis Harrison'
+__version__ = '0.1'
+__description__ = 'iPython Tools for Qiime-Matr-QC'
 __all__ = ["analysis","qc","ipyTools","flotplot"]
