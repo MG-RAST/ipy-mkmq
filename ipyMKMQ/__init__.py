769be8694243d9e331b6625ed706432207f5bcc5
Travis Harrison <guumby@gmail.com>
2013-01-29 14:22:27 -0600

    changed loads

--- ipyMKMQ/__init__.py
+++ ipyMKMQ/__init__.py
@@ -6,11 +6,3 @@ __author__ = 'Travis Harrison'
 __version__ = '0.2'
 __description__ = 'iPython Tools for Qiime-Matr-QC'
 __all__ = ["analysis","qc","ipyTools","flotplot","retina","metagenome","project","collection"]
-
-from analysis import Analysis, AnalysisSet
-from project import Project
-from collection import Collection
-from metagenome import Metagenome
-from qc import QC, Drisee, NucleoProfile, Rarefaction, merge_drisee_profile
-from retina import Retina
-from flotplot import FlotPlot
