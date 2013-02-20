1eaad6a5d9ca1a98d9e2bc6cc3741fb6b7c29340
Travis Harrison <guumby@gmail.com>
2013-02-20 14:20:01 -0600

    added wrapper function for caching plant data

--- ipyMKMQ/plant.py
+++ ipyMKMQ/plant.py
@@ -1,18 +1,61 @@
 #!/usr/bin/env python
 
+import hashlib, traceback
 import expression, genopheno, networks, ontology
 from ipyTools import *
-#import cdmi
+
+def get_plant_set(gids=[], def_name=None):
+    """Wrapper for Plant object creation, checks if cache (created through unique option set) exists first and returns that.
+    Returns a dict of plant_id : plant_obj
+    
+    see: help(Plant)
+    """
+    if not gids:
+        sys.stderr.write("No ids inputted\n")
+        return
+    cache_id  = "_".join(sorted(gids))
+    cache_md5 = hashlib.md5(cache_id).hexdigest()
+    cache_obj = load_object(cache_md5)
+    if cache_obj is not None:
+        print "Loading Plants for selected genomes from cached object"
+        return cache_obj
+    else:
+        # hack to get variable name
+        if def_name == None:
+            (filename,line_number,function_name,text)=traceback.extract_stack()[-2]
+            def_name = text[:text.find('=')].strip()
+        print "Loading Plants for selected genomes through API. Please wait, this may take several minutes ..."
+        new_obj = dict([(x, Plant(genome_id=x, def_name="%s['%s']"%(def_name, x))) for x in gids])
+        save_object(new_obj, cache_md5)
+        print "Done loading through API"
+        return new_obj
 
 # class for plants
 class Plant(object):
-    def __init__(self, genome_id):
+    """Class for working with Plant genomes
+    
+    self.genome_id   : kbase genome id of plant
+    self.EXPRESSION  : expression.PlantExpression object
+    self.GENOPHENO   : genopheno.Genotype_PhenotypeAPI object
+    self.NETWORKS    : networks.KBaseNetworks object
+    self.ONTOLOGY    : ontology.Ontology object
+    self.experiments : result of self.GENOPHENO.get_experiments(self.genome_id)
+    self.traits      : result of self.GENOPHENO.get_traits(self.experiments[0])
+    
+    """
+    def __init__(self, genome_id, def_name=None):
+        self.genome_id   = genome_id
         self.EXPRESSION  = expression.PlantExpression(Ipy.EXPRESSION_URL)
         self.GENOPHENO   = genopheno.Genotype_PhenotypeAPI(Ipy.GENOPHENO_URL)
         self.NETWORKS    = networks.KBaseNetworks(Ipy.NETWORKS_URL)
         self.ONTOLOGY    = ontology.Ontology(Ipy.ONTOLOGY_URL)
-        self.experiments = self.GENOPHENO.get_experiments(genome_id)
+        self.experiments = self.GENOPHENO.get_experiments(self.genome_id)
         self.traits      = self.GENOPHENO.get_traits(self.experiments[0])
+        # hack to get variable name
+        if def_name == None:
+            (filename,line_number,function_name,text)=traceback.extract_stack()[-2]
+            def_name = text[:text.find('=')].strip()
+        self.defined_name = def_name
 
     def get_variations(self, count=5):
         return self.GENOPHENO.traits_to_variations(self.traits[0], count)
